c516d621dbfdfb8420a1a06691d3f40cc0bce21a
Suzan <sys612603@gmail.com>
2023-05-31 01:46:18 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
-
+import Contact from "./components/contact/Contact";
+import Home from "./components/home/Home";
+import Change from "./components/change/Change";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+      <Contact />
+      <Change />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/change/Change.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Change.css";
+const Change = () => {
+  return <div>Change</div>;
+};
+
+export default Change;
